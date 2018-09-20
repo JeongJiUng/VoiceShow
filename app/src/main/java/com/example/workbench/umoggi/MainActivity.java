b803b235d36ecd4eaf13b2d9798179d49b774a5c
Jeong ji ung <anarkia5836@gmail.com>
2018-09-20 16:33:40 +0900

    기본전화기능 추가
    기본 전화 기능 추가.
    참고 : http://mainia.tistory.com/4884

--- app/src/main/java/com/example/workbench/umoggi/MainActivity.java
+++ app/src/main/java/com/example/workbench/umoggi/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.workbench.umoggi;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private Button          mDailogCall;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent("android.intent.action.DIAL"));
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
     }
 }
