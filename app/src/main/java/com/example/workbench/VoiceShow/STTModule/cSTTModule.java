005f46f2bd736d30b202c3ee6d0c8a4f37df3360
Jeong ji ung <anarkia5836@gmail.com>
2018-10-09 02:30:37 +0900

    Modified cSTTModule
    deleted permission check function

--- app/src/main/java/com/example/workbench/VoiceShow/STTModule/cSTTModule.java
+++ app/src/main/java/com/example/workbench/VoiceShow/STTModule/cSTTModule.java
@@ -1,12 +1,5 @@
 package com.example.workbench.VoiceShow.STTModule;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.os.Build;
-import android.support.v4.content.ContextCompat;
-
-
 import com.google.cloud.speech.v1p1beta1.RecognitionAudio;
 import com.google.cloud.speech.v1p1beta1.RecognitionConfig;
 import com.google.cloud.speech.v1p1beta1.RecognitionConfig.AudioEncoding;
@@ -25,16 +18,5 @@ public class cSTTModule
     public void Initialize()
     {
         //TODO:: Google Cloud to Text 기능 초기화
-    }
-
-    public void SetPermission(Context _context)
-    {
-        //TODO:: Android Permission 등록 및 확인
-        // Permission : INTERNET , RECORD_AUDIO
-        // 퍼미션 체크. 안드로이드 버전 23 이상이면 퍼미션 체크하는 알림창을 띄워줘야 한다.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        {
-            int             permissionResult = ContextCompat.checkSelfPermission(_context, Manifest.permission.INTERNET);
-        }
     }
 }
