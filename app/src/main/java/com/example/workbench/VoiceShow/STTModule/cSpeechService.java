616bb9d4099d2648d3332d421765b1000c1f86b6
Jeong ji ung <anarkia5836@gmail.com>
2018-10-19 20:45:05 +0900

    Modified cSpeechService
    Access Token 작업 관련에서 막힘. 망함...

--- app/src/main/java/com/example/workbench/VoiceShow/STTModule/cSpeechService.java
+++ app/src/main/java/com/example/workbench/VoiceShow/STTModule/cSpeechService.java
@@ -1,18 +1,28 @@
 package com.example.workbench.VoiceShow.STTModule;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
+import com.google.auth.oauth2.AccessToken;
 import com.google.cloud.speech.v1.RecognizeResponse;
 import com.google.cloud.speech.v1.SpeechGrpc;
 import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
@@ -21,6 +31,8 @@ import com.google.cloud.speech.v1.StreamingRecognitionResult;
 import com.google.cloud.speech.v1.StreamingRecognizeResponse;
 import com.google.cloud.speech.v1.StreamingRecognizeRequest;
 import com.google.cloud.speech.v1.*;
+
+import com.google.protobuf.ByteString;
 
 import io.grpc.ManagedChannel;
 import io.grpc.stub.StreamObserver;
@@ -195,12 +207,127 @@ public class cSpeechService extends Service
     private String getDefaultLanguageCode()
     {
         final Locale        locale = Locale.getDefault();
+        final StringBuilder language = new StringBuilder(locale.getLanguage());
+        final String        country = locale.getCountry();
+
+        if (!TextUtils.isEmpty(country))
+        {
+            language.append("-");
+            language.append(country);
+        }
+        return language.toString();
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent)
     {
-        return null;
+        return mBinder;
+    }
+
+    public void addListener(@NonNull Listener listener)
+    {
+        mListeners.add(listener);
+    }
+
+    public void removeListener(@NonNull Listener listener)
+    {
+        mListeners.remove(listener);
+    }
+
+    /**
+     * Starts recognizing speech audio.
+     *
+     * @param sampleRate The sample rate of the audio.
+     */
+    public void startRecognizing(int sampleRate)
+    {
+        if (mApi == null)
+        {
+            Log.w(TAG, "API not ready. Ignoring the request.");
+            return;
+        }
+        // Configure the API
+        mRequestObserver = mApi.streamingRecognize(mResponseObserver);
+        mRequestObserver.onNext(StreamingRecognizeRequest.newBuilder().setStreamingConfig(StreamingRecognitionConfig.newBuilder().setConfig(RecognitionConfig.newBuilder()
+                                .setLanguageCode(getDefaultLanguageCode()).setEncoding(RecognitionConfig.AudioEncoding.LINEAR16).setSampleRateHertz(sampleRate).build())
+                                .setInterimResults(true).setSingleUtterance(true).build()).build());
+    }
+
+    /**
+     * Recognizes the speech audio. This method should be called every time a chunk of byte buffer
+     * is ready.
+     *
+     * @param data The audio data.
+     * @param size The number of elements that are actually relevant in the {@code data}.
+     */
+    public void recognize(byte[] data, int size)
+    {
+        if (mRequestObserver == null)
+        {
+            return;
+        }
+        // Call the streaming recognition API
+        mRequestObserver.onNext(StreamingRecognizeRequest.newBuilder().setAudioContent(ByteString.copyFrom(data, 0, size)).build());
+    }
+
+    /**
+     * Recognize all data from the specified {@link InputStream}.
+     *
+     * @param stream The audio data.
+     */
+    public void recognizeInputStream(InputStream stream)
+    {
+        try
+        {
+            mApi.recognize(RecognizeRequest.newBuilder().setConfig(RecognitionConfig.newBuilder().setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                                    .setLanguageCode("en-US").setSampleRateHertz(16000).build()).setAudio(RecognitionAudio.newBuilder().setContent(ByteString.readFrom(stream))
+                                    .build()).build(), mFileResponseObserver);
+        }
+        catch (IOException e)
+        {
+            Log.e(TAG, "Error loading the input", e);
+        }
+    }
+
+    private class SpeechBinder extends Binder
+    {
+        cSpeechService getService()
+        {
+            return cSpeechService.this;
+        }
+    }
+
+    private final Runnable mFetchAccessTokenRunnable = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            fetchAccessToken();
+        }
+    };
+
+    private class AccessTokenTask extends AsyncTask<Void, Void, AccessToken>
+    {
+        @Override
+        protected AccessToken doInBackground(Void... voids)
+        {
+            final SharedPreferences prefs = getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+            String          tokenValue = prefs.getString(PREF_ACCESS_TOKEN_VALUE, null);
+            long            expirationTime = prefs.getLong(PREF_ACCESS_TOKEN_EXPIRATION_TIME, -1);
+
+            // Check if the current token is still valid for a while
+            if (tokenValue != null && expirationTime > 0)
+            {
+                if (expirationTime > System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TOLERANCE)
+                {
+                    return new AccessToken(tokenValue, new Date(expirationTime));
+                }
+            }
+
+            // TODO:: Access Token
+
+            return null;
+        }
     }
 }
