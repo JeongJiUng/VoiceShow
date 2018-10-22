86ed3e137de4ab1e723ee4dcf5fc898151d00bda
Jeong ji ung <anarkia5836@gmail.com>
2018-10-22 18:10:51 +0900

    Added cSTTModuleManager
    기능 추가 완료. 테스트 필요.
    STT에 관한 Cloude Speech service, 음성녹음 기능 관리

--- app/src/main/java/com/example/workbench/VoiceShow/STTModule/cSTTModuleManager.java
+++ app/src/main/java/com/example/workbench/VoiceShow/STTModule/cSTTModuleManager.java
@@ -1,7 +1,18 @@
 package com.example.workbench.VoiceShow.STTModule;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.widget.RecyclerView;
+
 public class cSTTModuleManager
 {
+    private Activity        mActivity;
     private cSpeechService  mSpeechService;
     private cVoiceRecorder  mVoiceRecorder;
 
@@ -29,6 +40,78 @@ public class cSTTModuleManager
             super.onVoiceEnd();
             if (mSpeechService != null)
                 mSpeechService.finishRecognizing();
+        }
+    };
+
+    private RecyclerView mRecyclerView;
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            mSpeechService  = cSpeechService.from(service);
+            mSpeechService.addListener(mSpeechServiceListener);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            mSpeechService = null;
+        }
+    };
+
+    public void onStart()
+    {
+        // Prepare Cloud Speech API
+        mActivity.bindService(new Intent(mActivity, cSpeechService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
+
+        // Start listening to voices
+        startVoiceRecorder();
+    }
+
+    public void onStop()
+    {
+        // Stop listening to voice
+        stopVoiceRecorder();
+        mSpeechService.removeListener(mSpeechServiceListener);
+        mActivity.unbindService(mServiceConnection);
+        mSpeechService      = null;
+    }
+
+    public void onSaveInstanceState(Bundle outState)
+    {
+
+    }
+
+    private void startVoiceRecorder()
+    {
+        if (mVoiceRecorder != null)
+        {
+            mVoiceRecorder.stop();
+        }
+        mVoiceRecorder      = new cVoiceRecorder(mVoiceCallback);
+        mVoiceRecorder.start();
+    }
+
+    private void stopVoiceRecorder()
+    {
+        if (mVoiceRecorder != null)
+        {
+            mVoiceRecorder.stop();
+            mVoiceRecorder  = null;
+        }
+    }
+
+    private final cSpeechService.Listener mSpeechServiceListener = new cSpeechService.Listener()
+    {
+        @Override
+        public void onSpeechRecognized(String _text, boolean _isFinal)
+        {
+            if (_isFinal)
+            {
+                mVoiceRecorder.dismiss();
+            }
         }
     };
 }
