628a2ccd6bdec66dcce72adaf73666727d13af17
Jeong ji ung <anarkia5836@gmail.com>
2018-11-02 01:42:01 +0900

    Modify Telephony Service
    통화 상태 감지 및 통화 중 음성인식 기능 활성화

--- app/src/main/java/com/example/workbench/VoiceShow/cSystemManager.java
+++ app/src/main/java/com/example/workbench/VoiceShow/cSystemManager.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 
 import com.example.workbench.VoiceShow.STTModule.cSTTModuleManager;
-import com.example.workbench.VoiceShow.TelephonyModule.cCallCheckReceiver;
 
 public class cSystemManager
 {
@@ -24,8 +23,6 @@ public class cSystemManager
 
     private cSTTModuleManager   mSTTModule;
 
-    private cCallCheckReceiver  mCallReceiver;
-
     public void Initialize(Activity _act, Context _cont)
     {
         mActivity           = _act;
@@ -33,7 +30,6 @@ public class cSystemManager
 
         // initialize Modules
         mSTTModule          = new cSTTModuleManager();
-        mCallReceiver       = new cCallCheckReceiver();
     }
 
     public Activity GetActivity()
@@ -48,9 +44,5 @@ public class cSystemManager
     public cSTTModuleManager GetSTTModule()
     {
         return mSTTModule;
-    }
-    public cCallCheckReceiver GetCallReceiver()
-    {
-        return mCallReceiver;
     }
 }
