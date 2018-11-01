d67596d0028b8626c2d60867777d03a3f66648d6
Jeong ji ung <anarkia5836@gmail.com>
2018-11-02 02:11:26 +0900

    Add Service, Receiver
    어플리케이션이 종료되어 있어도 백그라운드에서 음성인식을 하기 위한 서비스와 리시버 등록 작업

--- app/src/main/java/com/example/workbench/VoiceShow/Util/cCallBroadcastReceiver.java
+++ app/src/main/java/com/example/workbench/VoiceShow/Util/cCallBroadcastReceiver.java
@@ -8,6 +8,13 @@ import android.os.Looper;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+/**
+ * 단말기의 통화 상태를 감지하기 위해 리시버를 사용하여 브로드케스팅 한다.
+ * 통화의 상태에 따른 처리를 이곳에서 한다.
+ * CALL_STATE_IDLE : 아무 행동도 없는 상태
+ * CALL_STATE_RINGING : 전화가 오고 있는 상태
+ * CALL_STATE_OFFHOOK : 전화를 걸거나, 전화중인 상태(통화 시작)
+ */
 public class cCallBroadcastReceiver extends BroadcastReceiver
 {
     String                  TAG = "PHONE STATE";
@@ -19,12 +26,20 @@ public class cCallBroadcastReceiver extends BroadcastReceiver
     {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        Log.d(TAG, "onReceive()");
+        Log.i(TAG, "onReceive()");
         String              state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 
-        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state))
+        if (state.equals(TelephonyManager.EXTRA_STATE_IDLE))
         {
-            
+
+        }
+        else if (state.equals(TelephonyManager.EXTRA_STATE_RINGING))
+        {
+
+        }
+        else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))
+        {
+
         }
 
         throw new UnsupportedOperationException("Not yet implemented");
--- app/src/main/java/com/example/workbench/VoiceShow/Util/cCallBroadcastService.java
+++ app/src/main/java/com/example/workbench/VoiceShow/Util/cCallBroadcastService.java
@@ -4,6 +4,10 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+/**
+ * 앱이 실행중이지 않은 경우에도 기능을 실행하기 위한 서비스
+ * 음성인식 모듈에 대한 처리를 이곳에서 진행
+ */
 public class cCallBroadcastService extends Service
 {
     String                  TAG = "PHONE STATE";
