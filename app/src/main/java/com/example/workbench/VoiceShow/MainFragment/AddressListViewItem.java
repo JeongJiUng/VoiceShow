b448d9f61d1373e3ec807273df60768935c201e4
sangbeomLee <wing951@naver.com>
2018-11-04 16:50:08 +0900

    add ListViewItem for addressFragment
    add Item

---
+++ app/src/main/java/com/example/workbench/VoiceShow/MainFragment/AddressListViewItem.java
@@ -0,0 +1,30 @@
+package com.example.workbench.VoiceShow.MainFragment;
+
+import android.graphics.drawable.Drawable;
+
+public class AddressListViewItem {
+
+    private Drawable iconDrawable;
+    private String name;
+    private String number;
+
+    public void setIcon(Drawable icon){
+        iconDrawable = icon;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setNumber(String number){
+        this.number = number;
+    }
+
+    public Drawable getIcon(){
+        return this.iconDrawable;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getNumber(){
+        return this.number;
+    }
+}
