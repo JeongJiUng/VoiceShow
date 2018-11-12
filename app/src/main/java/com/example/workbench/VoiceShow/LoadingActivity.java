9e7be78cdafaf92ee10bdb6bd525b9b95c0c45bc
sangbeomLee <wing951@naver.com>
2018-11-12 17:16:52 +0900

    add LoadingLayout
    로딩화면 추가

---
+++ app/src/main/java/com/example/workbench/VoiceShow/LoadingActivity.java
@@ -0,0 +1,25 @@
+package com.example.workbench.VoiceShow;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        startLoading();
+    }
+
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000); //2000딜레이
+    }
+}
